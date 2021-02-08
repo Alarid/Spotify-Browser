ffd7127d9979fd6ff1ac7a743514c54de74dd477
Yohann Legrand <yoh.legrand@gmail.com>
2021-02-08 10:53:18 +0100

    style(albums): added comments

--- src/helpers/album.ts
+++ src/helpers/album.ts
@@ -1,6 +1,7 @@
-import { AlbumCover } from 'requests/albums/albums.request.types'
+import { AlbumArtist, AlbumCover } from 'requests/albums/albums.request.types'
 import humanizeDuration from 'humanize-duration'
 
+// Instanciate humanizer with custom language for short litteral units
 const shortEnHumanizer = humanizeDuration.humanizer({
   language: 'shortEn',
   languages: {
@@ -12,6 +13,12 @@ const shortEnHumanizer = humanizeDuration.humanizer({
   },
 })
 
+/**
+ * Get Album Cover
+ *
+ * @param {AlbumCover[]} images - album images fetched from the API
+ * @return {string} - the first image below 300px width if found, or first image, or placeholder
+ */
 export const getImage = (images: AlbumCover[]): string => {
   const image = images.find((img) => img.width <= 300)
   if (image) {
@@ -24,13 +31,31 @@ export const getImage = (images: AlbumCover[]): string => {
   }
 }
 
-export const getArtists = (artists: Array<{ name: string }>): string =>
+/**
+ * Get Album Artists
+ *
+ * @param {AlbumArtist[]} artists - array of artists fetched from the API
+ * @return {string} - a concatenation of all the artists names
+ */
+export const getArtists = (artists: AlbumArtist[]): string =>
   artists.map((artist) => artist.name).join(', ')
 
+/**
+ * Get Total Duration
+ *
+ * @param {AlbumTrack[]} tracks - album tracks fetched from the API
+ * @return {string} - a humanized total duration of the album (sum of all tracks durations)
+ */
 export const getTotalDuration = (tracks: AlbumTrack[]): string => {
   const durationMs = tracks.reduce((total, track) => total + track.duration_ms, 0)
   return shortEnHumanizer(durationMs, { units: ['h', 'm'], round: true, delimiter: ' ' })
 }
 
+/**
+ * Get Track Duration
+ *
+ * @param {number} durationMs - duration of the track in milliseconds
+ * @return {string} - a humanized duraction for the track
+ */
 export const getTrackDuration = (durationMs: number): string =>
   shortEnHumanizer(durationMs, { units: ['m', 's'], round: true, delimiter: ' ' })
